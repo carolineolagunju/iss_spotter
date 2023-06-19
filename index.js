641adbe32ab10aa48a88f86e75749b367084459c
carolineolagunju <olamidejr21@gmail.com>
2023-06-19 16:44:35 -0600

    added function fetchMyIP, fetchCoordsByIp & fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const {fetchMyIP} = require('./iss');
+const {fetchMyIP, fetchCoordsByIp, fetchISSFlyOverTimes} = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log(`It didn't work!` , error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log(`It didn't work!` , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:' , ip);
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+
+// fetchCoordsByIp('68.144.118.188', (error, coordinates) => {
+//   if (error) {
+//     console.log(`It didn't work!` , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:', coordinates);
+// });
+
+
+const coordinates = {
+  latitude: '-80',
+  longitude: '-23'
+};
+
+fetchISSFlyOverTimes(coordinates, (error, passTimes) => {
+  if (error) return console.log(`It didn't work!` , error);
+
+  console.log(`It worked : Returned flyover times:`, passTimes);
 });
--- iss.js
+++ iss.js
@@ -11,10 +11,61 @@ const fetchMyIP = function(callback) {
       return callback(Error(msg), null);
     }
     
+    //converting to structured obj && making the first args of the cb equate to null
     const data = JSON.parse(body);
     const ip = data.ip;
     callback(null, ip);
   });
 };
 
-module.exports = {fetchMyIP};
+
+
+const fetchCoordsByIp = (IP, callback) => {
+  //requesting IP
+  request(`http://ipwho.is/${IP}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    //converting to object && logging error message if success staus === false
+    const data = JSON.parse(body);
+
+    if (!data.success) {
+      const msg = `Success status was ${data.success}. Server message says: ${data.message} when fetching for IP: ${data.ip}`;
+      callback(msg, null);
+      return;
+    }
+
+    //retrieving the latitude and longitude value as an object
+    const result = {
+      latitude: data.latitude,
+      longitude: data.longitude
+    };
+  
+    //calling cb function to print latitude and longitude
+    callback(null, result);
+  });
+};
+
+
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
+
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      const message = `Status Code ${response.statusCode} when fetching ISS pass times: ${body}`;
+      return callback(message, null);
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
+module.exports = {fetchMyIP, fetchCoordsByIp, fetchISSFlyOverTimes};
