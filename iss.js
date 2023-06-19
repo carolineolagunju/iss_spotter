04fac9c841c00b2e9f035065c4669f7f69e27f1f
carolineolagunju <olamidejr21@gmail.com>
2023-06-19 02:05:13 -0600

    lint and cleanup code

--- iss.js
+++ iss.js
@@ -1,13 +1,18 @@
 const request = require('request');
+
+
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org/?format=json', (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
+
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
     }
     
     const data = JSON.parse(body);
-    const ip = data;
+    const ip = data.ip;
     callback(null, ip);
   });
 };
